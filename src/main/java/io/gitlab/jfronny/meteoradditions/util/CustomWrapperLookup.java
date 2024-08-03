5b4feed4bfdf551875260f00135528bf88715ea8
JFronny <git@akbvopenfl1.hopto.org>
2024-08-03 14:22:46 +0200

    chore: directly delegate to registry manager

--- src/main/java/io/gitlab/jfronny/meteoradditions/util/CustomWrapperLookup.java
+++ src/main/java/io/gitlab/jfronny/meteoradditions/util/CustomWrapperLookup.java
@@ -2,10 +2,7 @@ package io.gitlab.jfronny.meteoradditions.util;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.*;
 
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -13,14 +10,16 @@ import java.util.stream.Stream;
 public class CustomWrapperLookup implements RegistryWrapper.WrapperLookup {
     @Override
     public Stream<RegistryKey<? extends Registry<?>>> streamAllRegistryKeys() {
-        return Registries.REGISTRIES.stream().map(Registry::getKey);
+        return getRegistryManager().stream().flatMap(DynamicRegistryManager::streamAllRegistryKeys);
     }
 
     @Override
     public <T> Optional<RegistryWrapper.Impl<T>> getOptionalWrapper(RegistryKey<? extends Registry<? extends T>> registryRef) {
+        return getRegistryManager().flatMap(s -> s.getOptionalWrapper(registryRef));
+    }
+
+    private Optional<DynamicRegistryManager> getRegistryManager() {
         return Optional.ofNullable(MinecraftClient.getInstance().player)
-                .map(Entity::getRegistryManager)
-                .flatMap(s -> s.getOptional(registryRef))
-                .map(Registry::getReadOnlyWrapper);
+                .map(Entity::getRegistryManager);
     }
 }
