b3a80693ab50dc02f5030bfcc37fd4c19d634dae
JFronny <git@akbvopenfl1.hopto.org>
2026-01-14 21:52:46 +0100

    fix: adjust keywords for 1.21.11
    Closes #41

--- src/main/java/io/gitlab/jfronny/meteoradditions/mixin/ModulesMixin.java
+++ src/main/java/io/gitlab/jfronny/meteoradditions/mixin/ModulesMixin.java
@@ -1,20 +1,21 @@
 package io.gitlab.jfronny.meteoradditions.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import io.gitlab.jfronny.meteoradditions.util.IModule;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(Modules.class)
 public class ModulesMixin {
-    @Redirect(
-            method = "searchTitles(Ljava/lang/String;)Ljava/util/Set;",
-            at = @At(value = "FIELD", target = "Lmeteordevelopment/meteorclient/systems/modules/Module;title:Ljava/lang/String;"),
-            remap = false
+    @ModifyArg(
+            method = "searchTitles(Ljava/lang/String;)Ljava/util/List;",
+            at = @At(value = "INVOKE", target = "Lmeteordevelopment/meteorclient/utils/Utils;searchLevenshteinDefault(Ljava/lang/String;Ljava/lang/String;Z)I"),
+            index = 0
     )
-    private String modifySearchTitlesArg(Module instance) {
-        return ((IModule) instance).meteorAdditions$getQueryString();
+    String modifySearchTitlesArg(String text, @Local(name = "module") Module module) {
+        return text.equals(module.title) ? ((IModule) module).meteorAdditions$getQueryString() : text;
     }
 }
